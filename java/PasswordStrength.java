a8d95b052c197cd81db62f36054776cd31698c8f
TimoSLE <timo-s@fn.de>
2020-10-11 00:55:01 +0200

    Add Password Strength Utility

---
+++ java/PasswordStrength.java
@@ -0,0 +1,40 @@
+public class PasswordStrength {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int passwordStrength(String password) {
+        int iPasswordScore = 0;
+
+        if( password.length() < 8 )
+            iPasswordScore = 0;
+        else if( password.length() >= 10 )
+            iPasswordScore += 2;
+        else
+            iPasswordScore += 1;
+
+        if( password.matches("(?=.*[0-9].*[0-9]).*") )
+            iPasswordScore += 2;
+        else if ( password.matches("(?=.*[0-9]).*") )
+            iPasswordScore += 1;
+
+        if( password.matches("(?=.*[a-z]).*") )
+            iPasswordScore += 2;
+
+        if( password.matches("(?=.*[A-Z].*[A-Z]).*") )
+            iPasswordScore += 2;
+        else if( password.matches("(?=.*[A-Z]).*") )
+            iPasswordScore += 1;
+
+        if( password.matches("(?=.*[~!@#$%^&*()_-].*[~!@#$%^&*()_-]).*") )
+            iPasswordScore += 2;
+        else if( password.matches("(?=.*[~!@#$%^&*()_-]).*") )
+            iPasswordScore += 1;
+
+        return iPasswordScore;
+
+    }
+
+}
+
